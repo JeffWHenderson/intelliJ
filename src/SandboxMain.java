16370308d1b495a8e07cb070ba2e360431c163a7
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-20 19:58:50 -0400

    wrote some code to test the time it takes to run Array List and Linked lists.

--- src/SandboxMain.java
+++ src/SandboxMain.java
@@ -1,19 +1,35 @@
-class Machine {
-    public void start() {
-        System.out.println("snapping a picture");
-    }
-}
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SandboxMain {
     public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
-        Machine machine1 = new Machine() {
-            @Override
-            public void start() {
-                System.out.println("running as anonymously");
-            }
-        };
+        doTimings("array List", arrayList);
+        doTimings("linked List", linkedList);
+    }
 
-        machine1.start();
+    private static void doTimings(String type, List<Integer> list) {
+
+        for(int i = 0; i < 1E5; i++){
+            list.add(i);
+        }
+
+        long start = System.currentTimeMillis();
+
+        // time for adding to end of list   ARRAY LIST MUCH FASTER
+//        for(int i = 0; i<1E5; i++){
+//            list.add(i);
+//        }
+        // time for adding to beginning of list
+        for(int i = 0; i<1E5; i++){
+            list.add(0, i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("time elapsed: " + (start - end) + " ms for " + type);
     }
 }
